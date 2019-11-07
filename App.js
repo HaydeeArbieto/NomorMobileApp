9a146439f554b9a88a95d2d11356137c18a3610a
Arbieto, Haydee <haydee.arbieto@sogeti.com>
2019-11-07 17:16:18 +0100

    Change frontend in the first side

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import HeaderImage from './logo_image.js';
 import Button from 'react-native-button';
 //import { Button } from 'react-bootstrap';
@@ -11,12 +11,11 @@ export default class App extends React.Component {
    render() {
       return (
          <View style = {styles.container} >
-          <HeaderImage style = {styles.logoContainer} ></HeaderImage>
+            <HeaderImage style = {styles.logoContainer,'\n'} ></HeaderImage>
+            <View style={ styles.hrLine}/>
             <Text style = {styles.textContainer}>Identifera skadedjur med bild</Text>
-            <Text style = {styles.textContainer}>Ta en skarp och ljus bild</Text>
-
-
-            <Image source = {require('./src/images/mainPicture.jpg')} style = {{ width: 150, height: 220 }}/>
+            <Text style = {styles.textContainer}> Ta en skarp och ljus bild</Text>
+            <Image source = {require('./src/images/mainPicture.jpg')} style = {{ width: 150, height: 220, marginTop: 5 }}/>
             <Button style = {styles.buttonContainer}>
                SCANNA SKADEDJUR
             </Button>
@@ -25,25 +24,39 @@ export default class App extends React.Component {
    }
 }
 
+const { width } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
    container: {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'flex-start',
+      marginTop: 20
    },
    textContainer: {
-      fontSize: 10
+      fontSize: 10,
+      marginTop: 10
 
    },
    buttonContainer: {
          fontSize: 8,
-         color: 'white',
-         backgroundColor: 'orange'
+         color: '#fff',
+         backgroundColor: 'rgba(239,121,38,1)',
+         marginTop: 5
+         /*borderWidth: 3,
+         borderColor: '#fff'*/
    },
    logoContainer: {
        width: 100,
-       height: 30
+       height: 30,
+       marginTop: 10
+    },
+    hrLine: {
+         backgroundColor: 'rgba(239,121,38,1)', /*'#ff6600'*/
+          height: 6,
+          width: 165,
+          marginTop: 10
     }
 });
 
